647c4c83e866f120672ff4b28b22fd08b345de1a
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2025-02-02 22:06:09 +0100

    init

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# ------------------------------------------------------
+# copy fonts
+# ------------------------------------------------------
+sudo cp -a "$builddir"/fonts/* /usr/share/fonts
+sudo fc-cache -f -v
+fc-cache -f -v
+
+# ------------------------------------------------------
+# copy new file templates
+# ------------------------------------------------------
+mkdir -p "$HOME/Templates"
+sudo cp -a "$builddir"/Templates/* "$HOME"/Templates
+
+# icons
+sudo tar -xvf "$builddir"/icons/Colloid.tar.xz -C /usr/share/icons
+
+# config
+cp -a "$builddir"/config/* "$HOME"/.config
+
+
+echo "Done!"
+
+
+
+
